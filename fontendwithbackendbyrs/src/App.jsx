cfc688bc41575dcb153510fcf1f524e771108220
Rahul Shinde <82260071+rahul0901@users.noreply.github.com>
2023-12-03 00:29:45 +0530

    adddd

--- fontendwithbackendbyrs/src/App.jsx
+++ fontendwithbackendbyrs/src/App.jsx
@@ -1,6 +1,7 @@
 import {Routes, Route} from 'react-router-dom';
 import Homepage from './Components/Homepage';
 import Register from './Components/Register';
+import Login from './Components/Login';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route exact path='/' element={<Homepage />} />
         <Route exact path='/register' element={<Register />} />
+        <Route exact path='/login' element={<Login />} />
       </Routes>
     </div>
   );
