b167f9f09c05453776b940317a23cdfc5555c640
Rahul Shinde <82260071+rahul0901@users.noreply.github.com>
2023-12-03 00:29:57 +0530

    adddddd

--- fontendwithbackendbyrs/src/Components/Homepage.jsx
+++ fontendwithbackendbyrs/src/Components/Homepage.jsx
@@ -1,7 +1,18 @@
+import { useNavigate } from "react-router-dom";
+
 function Homepage(){
+
+    const route = useNavigate();
+
+    function goToLogin(){
+        route('/login')
+    }
+
     return(
         <>
             <div>HomePage</div>
+            <button onClick={()=>route('/register')}>Register Here</button>
+            <button onClick={goToLogin}>Login Here</button>
         </>
     )
 }
